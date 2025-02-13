62bf65013f55ca56c01b535604372de30352fd9e
Chirag Singh <cschiragsingh99@gmail.com>
2025-02-13 18:05:12 +0530

    commit

--- Character_pattern.java
+++ Character_pattern.java
@@ -1,9 +1,14 @@
 public class Character_pattern {
     public static void main(String[] args) {
         int n = 4;
-        char ch = "A";
+        char ch = 'A';
         //Outer Loop
-        for(int i=1 ; i<=n ; i++ ) {
+        for(int i=1 ; i<=n ; i++ ) {     // i = lines
+            //inner loop
+            for (int j=1 ; j<=i ; j++){  // j = char
+                    System.out.print(ch);
+                    ch++;
+            }System.out.println();
             
         }
     }
