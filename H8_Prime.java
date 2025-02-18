0a954c5c3d410d4606fb2c8b35865b5cc8cc1543
Chirag Singh <cschiragsingh99@gmail.com>
2025-02-18 19:42:02 +0530

    Prime

---
+++ H8_Prime.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class H8_Prime {
+     // WAP to check a no n is Prime or not
+        
+        // only for n>=2
+        public static boolean isPrime(int n){ 
+            if (n==2){
+                return true;
+            }          
+            boolean isPrime = true;
+            for (int i=2 ; i<=n-1 ; i++){          // i<= Math.sqrt(n) [For more optimized solution]
+                if (n % i==0){ 
+                    isPrime = false;
+                    break;
+                }
+            } 
+
+            return isPrime;
+        }
+        public static void main (String args[]) {
+            System.out.println(isPrime(4));
+    }
+}  
+
