bf6e2152d65356b6a33c6baa07c1548952d3872b
Chirag Singh <cschiragsingh99@gmail.com>
2025-02-21 23:01:42 +0530

    Commit

---
+++ i1_HollowRec_Pattern.java
@@ -0,0 +1,20 @@
+public class i1_HollowRec_Pattern {
+    public static void hollow_rec(int totRows ,int totCols) {
+        for(int i=1 ; i<= totRows ; i++) {  //outer loop
+            for (int j=1 ; j<=totCols ; j++) { //inner - column
+                // cell - (i,j)
+                if(i == 1 || i == totRows || j == 1 || j == totCols)  {
+                //boundar cells
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }   
+            }
+            System.out.println();
+        }
+    }
+    
+    public static void main(String[] args) {
+        hollow_rec(4,5 );
+    }    
+}
